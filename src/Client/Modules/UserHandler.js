381822a9f89e2d49d72b02c44fe508916085a1ee
sean <sean.mcbroom@outlook.com>
2021-08-17 16:03:42 -0500

    Use discord events in UserHandler

--- src/Client/Modules/UserHandler.js
+++ src/Client/Modules/UserHandler.js
@@ -17,46 +17,66 @@ class UserHandler {
 
     setup() {
         this.client.once('ready', () => {
-            // this.client.on('guildMemberRemove', (member) => {
-            //     if (!this.isUserInAGuild(member.id)) {
-            //         this.Remove(member.id);
-            //     }
-            // });
+            this.loadAll();
+
+            this.client.on('guildMemberAdd', (member) => {
+                this.Add(member.user);
+            })
+
+            this.client.on('guildMemberRemove', (member) => {
+                if (!this.isUserInAGuild(member.id)) {
+                    this.Remove(member.user);
+                }
+            });
         });
     }
 
-    // isUserInAGuild(userId) {
-    //     for (const guild of this.client.guilds._cache) {
-    //         for (const member of guild[1].members._cache) {
-    //             if (member[1].user.id == userId) {
-    //                 return true;
-    //             }
-    //         }
-    //     }
+    isUserInAGuild(userId) {
+        for (const guild of this.client.guilds._cache) {
+            for (const member of guild[1].members._cache) {
+                if (member[1].user.id == userId) {
+                    return true;
+                }
+            }
+        }
 
-    //     return false;
-    // }
+        return false;
+    }
 
     Add(userData) {
-        const user = new User(this.client, userData, {
-            DataFormat: this.DataFormat
-        });
+        if (!this._cache.get(userData.id) && !this.bot) {
+            //console.log(`Added User ${userData.username}`)
+            const user = new User(this.client, userData, {
+                DataFormat: this.DataFormat
+            });
 
-        this._cache.set(userData.id, user);
+            this._cache.set(userData.id, user);
 
-        return user;
+            return user;
+        }
     }
 
     Remove(userData) {
+        //console.log(`Removed User ${userData.username}`)
         return this._cache.delete(userData.id);
     }
 
     Get(userData) {
         const user = this._cache.get(userData.id);
 
-        if (user) return user
+        if (user) {
+            return user
+        }
 
-        return this.Add(userData);
+        return this.Add(userData)
+    }
+
+    loadAll() {
+        for (const guild of this.client.guilds._cache) {
+            for (const member of guild[1].members._cache) {
+                this.Add(member[1].user);
+            }
+        }
     }
 }
 
