290eae36f2c52885430869c0a3486aa2ab218efd
sean <sean.mcbroom@outlook.com>
2021-09-25 18:57:08 -0500

    Remove general commands loading default

--- src/Client/main.js
+++ src/Client/main.js
@@ -19,7 +19,7 @@ class Client extends AkairoClient {
 
     this.interactionHandler = new InteractionHandler(this, {
       directory: './src/Client/Commands',
-      defaultCommandTags: []
+      defaultCommandTags: ['general']
     });
 
     this.listenerHandler = new ListenerHandler(this, {
