2b00175ec3f5321463c3f8ab8db6bd07de67e79d
sean <sean.mcbroom@outlook.com>
2021-09-25 18:58:19 -0500

    Add role bind updating

--- src/Client/Modules/GuildHandler.js
+++ src/Client/Modules/GuildHandler.js
@@ -90,22 +90,81 @@ class Guild {
         this.client.interactionHandler.loadInteractionsWithTagsOnGuild(this.id, ['premium', 'dev', 'management']);
     }
 
-    async updateMember(User) {
-        //todo: Update nickname, roles, verified stats, etc.
+    async updateMember(Member) {
+        const osuUser = await Member.user.getOsuUser();
+        const binds = await this.Datastore.getData('Binds');
+
+        if (binds && Object.keys(binds).length > 1) {
+            const variables = {
+                'TOP_PP': async () => {
+                    return await osuUser.getHighestPP();
+                },
+                'PP': async () => {
+                    return osuUser.pp_raw;
+                },
+                'RANK': async () => {
+                    return osuUser.pp_rank;
+                },
+                'CTRY_RANK': async () => {
+                    return osuUser.pp_country_rank;
+                },
+                'CTRY': async () => {
+                    return osuUser.country;
+                },
+                'RANKED_SCORE': async () => {
+                    return osuUser.ranked_score;
+                },
+                'SCORE': async () => {
+                    return osuUser.total_score;
+                },
+                'ACCURACY': async () => {
+                    return osuUser.accuracy;
+                },
+                'PLAYTIME': async () => {
+                    return osuUser.total_seconds_played;
+                },
+                'VERIFIED': async () => {
+                    return (osuUser ? true : false);
+                }
+            };
+
+            binds.forEach(async bind => {
+                const role = this.roles.cache.find(r => r.id === bind.ID);
+
+                if (role) {
+                    let condition = bind.C;
+
+                    for (const i in variables) {
+                        const regex = new RegExp(i, 'g');
+                        const value = await variables[i]();
+
+                        condition = condition.replace(regex, value);
+                    }
+
+                    const meetsRequirements = Util.evalStringExpression(condition);
+
+                    if (meetsRequirements) {
+                        Member.roles.add(role).catch(() => { });
+                    } else {
+                        Member.roles.remove(role).catch(() => { });
+                    }
+                }
+            });
+        }
     }
 
-    async postMemberOsuScore(User, Score, Gain) {
+    async postMemberOsuScore(Member, Score, Gain) {
         const trackingWhitelist = await this.Datastore.getData('trackingWhitelist');
         const trackingChannelId = await this.Datastore.getData('trackingChannel');
 
         const channel = this.channels.cache.find(c => c.id === trackingChannelId);
 
         if (!channel) return
-        if (!trackingWhitelist[User.id]) return
-        if (trackingWhitelist[User.id].top < Score.profile_rank) return
+        if (!trackingWhitelist[Member.id]) return
+        if (trackingWhitelist[Member.id].top < Score.profile_rank) return
 
         channel.send({
-            content: `New **#${Score.profile_index}** for ${User.username}!`,
+            content: `New **#${Score.profile_index}** for ${Member.username}!`,
             embeds: [
                 new Discord.MessageEmbed()
                     .setColor(this.client.Settings.Colors.Main)
