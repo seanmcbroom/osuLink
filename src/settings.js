248f0b90b6b0c94b9cf09aebb664ce550b0879f1
sean <sean.mcbroom@outlook.com>
2021-09-27 22:41:45 -0500

    Implement test client

--- src/settings.js
+++ src/settings.js
@@ -1,8 +1,12 @@
+const isProduction = process.env.PRODUCTION === "TRUE";
+
 module.exports = {
     Owners: [517529173061795840, 866759411267141682],
 
     Links: {
-        Invite: 'https://discord.com/api/oauth2/authorize?client_id=767176248811847691&permissions=2550262800&scope=bot%20applications.commands',
+        Invite: isProduction
+            ? 'https://discord.com/api/oauth2/authorize?client_id=767176248811847691&permissions=2550262800&scope=bot%20applications.commands'
+            : 'https://discord.com/api/oauth2/authorize?client_id=718794018468790293&permissions=2550262800&scope=bot%20applications.commands',
         SupportServer: 'https://discord.gg/K2dTCmJ',
         TopGG: 'https://top.gg/bot/767176248811847691'
     },
@@ -28,7 +32,9 @@ module.exports = {
         }
     },
 
-    BotToken: process.env.BOT_TOKEN,
+    BotToken: isProduction
+        ? process.env.BOT_TOKEN
+        : process.env.BOT_TOKEN_PTB,
 
     osuApiKey: process.env.OSU_API_KEY,
 
